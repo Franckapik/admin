fcf7fba46352340b553f71e55baf103db33672be
Franckapik <fanch44@hotmail.com>
2021-05-31 10:57:38 +0200

    adding logger winston

--- app.js
+++ app.js
@@ -6,58 +6,33 @@ const app = express();
 
 const config = require('./config');
 
-const Pool = require('pg').Pool
-const pool = new Pool(config.db)
+const knex = require('knex')(config.db);
+
+const logger = require('./log/logger');
+
 
 // Middleware since express 4.16
 app.use(express.json());
-
-
-pool.query('SELECT NOW() as now')
-  .then(res => console.log("Connection checked on database " + config.db.host + "/" + config.db.database ))
-  .catch(e => console.error("Connection Failed on database $condif.db.database " + e.stack))
-
-const db = {
-  query: (text, params) => pool.query(text, params)
-}
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get("/status", async (req, res) => {
-const essai = await db.query('select * from "status"');
-res.json(essai.rows);
-});
+knex
+.raw('SELECT NOW() as now')
+.then(res => console.log("Connexion checked on database",config.db.connection.host,"/",config.db.connection.database," [", res.rows[0].now,"] "))
+.catch(err => console.log(err))
 
-app.get("/db/:id", async (req, res) => {
-const {id} = req.params
-const essai = await db.query('select * from "product"');
-/* const essai = await db.query('select * from "product" where id = $1', [id])
- */
-
-res.json(essai.rows);
-});
-
-app.post('/newProduct', (req,res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.post("/newProduct", (req, res) => {
+  knex('product')
+  .insert(req.body)
+  .returning('product_id')
+  .then(id => {
+    logger.info('[Knex] Table ' + 'product' + ' Données enregistrées (id): %s', id[0]);
+    return id
+  }).catch(error => logger.error('[Erreur Enregistrement ' + 'product' + '] Sauvegarde db %s', error))
 })
 
-
-
-app.post('/addStatus', (req,res) => {
-  console.log(req.body);
-  const query = {
-    text : "INSERT INTO public.product (product_id, performance_id, name, price, img, stock, collection_id, packaging_id, property_id) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)",
-    values : Object.values([req.body.product_id, req.body.performance_id, req.body.name, req.body.price, req.body.img, req.body.stock, req.body.collection_id, req.body.packaging.id, req.body.property_id])
-  }
-  db.query(query)
-  .then(resp => 
-    res.send(resp.row))
-  .catch(e => console.error(e.stack))
-
-})
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
---
+++ log/logger.js
@@ -0,0 +1,73 @@
+const winston = require('winston');
+const env = process.env.NODE_ENV;
+/* Voici la hierarchie const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6
+};*/
+
+const logger = winston.createLogger({
+  level: env!== 'production' ? 'debug' : 'info', //le niveau affiché au npm start
+  format: winston.format.combine(
+  winston.format.colorize(),
+  winston.format.prettyPrint(),
+  winston.format.splat(),
+  winston.format.simple(),
+  winston.format.printf(
+    info =>
+      `${info.timestamp} ${info.level} : ${info.message}`
+  )
+),
+  transports: [
+    new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'log/combined.log' })
+  ]
+});
+
+if (env!== 'production') { //console dvp sans precision de level
+  logger.add(
+    new winston.transports.Console({
+    format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.colorize(),
+    winston.format.prettyPrint(),
+    winston.format.splat(),
+    winston.format.simple(),
+    winston.format.printf(
+      info =>
+        `${info.timestamp} ${info.level} : ${info.message}`
+    )
+  )
+  }));
+}
+
+if (env== 'production') { //console production avec le level warn
+  logger.add(
+    new winston.transports.Console({
+
+    level:'warn',
+    format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.colorize(),
+    winston.format.prettyPrint(),
+    winston.format.splat(),
+    winston.format.simple(),
+    winston.format.printf(
+      info =>
+        `${info.timestamp} ${info.level} : ${info.message}`
+    )
+  )
+  }));
+}
+
+logger.stream = {
+    write: function(message, encoding){
+        logger.verbose(message);
+    }
+};
+
+module.exports=logger;
